4519e774017bf3f881011013052e6f7e2d936150
dantlangston <dan.t.langston@gmail.com>
2021-12-01 13:07:45 -0500

    new content, general updates

--- gallery.js
+++
@@ -1,5 +0,0 @@
-
-const tl = gsap.timeline();
-
-tl.to(".galleryimage", {duration: .5, y:-25, ease: " sine.in", stagger:.25, opacity: 1});
-tl.to(".info", {duration: .5, ease: " sine.in", stagger:.25, opacity: 1} );
