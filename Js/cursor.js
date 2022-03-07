d10377822be519375f80f79271ca074126aa26f1
dantlangston <dan.t.langston@gmail.com>
2022-03-07 15:17:13 -0500

    custom cursor, slight styling

---
+++ Js/cursor.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function() {
+    $(document).on('mousemove', function(e) {
+      $('#circularcursor').css({
+        left: e.pageX,
+        top: e.pageY
+      });
+    })
+  });
