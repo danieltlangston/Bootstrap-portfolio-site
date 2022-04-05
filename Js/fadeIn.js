f8a5be3fc543c216e963b576554e72bcf416333e
dantlangston <dan.t.langston@gmail.com>
2022-04-05 13:07:01 -0400

    card image sizing, start of say update

--- Js/fadeIn.js
+++ Js/fadeIn.js
@@ -1,5 +1,5 @@
 
 const tl = gsap.timeline();
 
-tl.to(".fadeIn", {delay: .75, duration: .75, y:-25, ease: " sine.in", stagger:.25, opacity: 1});
-tl.to(".fadeIn", {duration: .5, ease: " sine.in", stagger:.25, opacity: 1} );
+tl.to(".fadeIn", {delay: .75, duration: .5, y:-25, ease: " sine.in", stagger:.1, opacity: 1});
+tl.to(".fadeIn", {duration: .25, ease: " sine.in", stagger:.1, opacity: 1} );
