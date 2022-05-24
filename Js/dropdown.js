aaad7e5f29f53ebadeffb5c830b47e953e294bb2
dantlangston <dan.t.langston@gmail.com>
2022-05-23 21:03:41 -0400

    syling overhaul, dropdown nav added

---
+++ Js/dropdown.js
@@ -0,0 +1,11 @@
+
+$('.menu').click(function(){
+    $(this).toggleClass('open');
+
+  });
+
+
+$('.dropdown').click(function(){
+  $(this).find('.dropdown-content').toggleClass('dropdown-content-display');
+
+});
--- Js/fadeIn.js
+++ Js/fadeIn.js
@@ -3,3 +3,8 @@ const tl = gsap.timeline();
 
 tl.to(".fadeIn", {delay: .75, duration: .5, y:-25, ease: " sine.in", stagger:.1, opacity: 1});
 tl.to(".fadeIn", {duration: .25, ease: " sine.in", stagger:.1, opacity: 1} );
+
+
+
+
+
